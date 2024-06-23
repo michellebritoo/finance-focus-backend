699487945e8aec59530ebecab59fc670dc48cb66
michellebritoo <dev.michellebrito@gmail.com>
2024-06-23 15:29:47 -0300

    feat: create enum class to bacen codes month - PF

---
+++ src/main/kotlin/br/com/michellebrito/financeFocusBackend/rates/model/CodeRatesMonth.kt
@@ -0,0 +1,8 @@
+package br.com.michellebrito.financeFocusBackend.rates.model
+
+enum class CodeRatesMonth(val id: String) {
+    CREDIT("25435"),
+    PROPRIETY_CREDIT("25472"),
+    VEHICLE_CREDIT("25471"),
+    HOUSE_CREDIT("25499")
+}
