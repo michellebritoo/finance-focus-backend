68421617a1ef0015b9d06fafb47cae53c63a371d
michellebritoo <dev.michellebrito@gmail.com>
2024-06-24 00:24:48 -0300

    feat: field validation

--- src/main/kotlin/br/com/michellebrito/financeFocusBackend/rates/model/RatesMonthModel.kt
+++ src/main/kotlin/br/com/michellebrito/financeFocusBackend/rates/model/RatesMonthModel.kt
@@ -1,8 +1,14 @@
 package br.com.michellebrito.financeFocusBackend.rates.model
 
+import org.jetbrains.annotations.NotNull
+
 data class RatesMonthModel(
+    @field:NotNull("Informe o valor")
     val amount: Double,
+    @field:NotNull("Informe a taxa de juros")
     val rateValue: Double,
+    @field:NotNull("Informe o numero de meses")
     val time: Int,
-    val index: Int
+    @field:NotNull("Informe o fator de busca")
+    val factor: Int
 )
