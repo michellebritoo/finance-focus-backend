e6ca6e73c6fc82410c11e63ad0874bed50aa15c8
michellebritoo <dev.michellebrito@gmail.com>
2024-10-27 15:16:12 -0300

    improvement: return update goal core function

--- src/main/kotlin/br/com/michellebrito/financeFocusBackend/goals/model/UpdateGoalRequest.kt
+++ src/main/kotlin/br/com/michellebrito/financeFocusBackend/goals/model/UpdateGoalRequest.kt
@@ -5,13 +5,6 @@ import jakarta.validation.constraints.NotBlank
 data class UpdateGoalRequest(
     @field:NotBlank(message = "Informe um id")
     var id: String,
-    var userUID: String? = null,
     var name: String? = null,
     var description: String? = null,
-    var totalValue: Float? = null,
-    var remainingValue: Float? = null,
-    var gradualProgress: Boolean? = null,
-    var monthFrequency: Boolean? = null,
-    var initDate: String? = null,
-    var finishDate: String? = null
 )
